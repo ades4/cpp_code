9205ad4a4538be04514992de87a2630b2c806edd
ades4 <1647635529@qq.com>
2020-07-28 21:22:29 +0800

    两种方法

--- 反转链表_Lk/反转链表.cpp
+++ 反转链表_Lk/反转链表.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
-
+#include <iostream>
+using namespace std;
 
 struct ListNode {
 	int val;
@@ -8,18 +9,50 @@ struct ListNode {
 			val(x), next(NULL) {
 	}
 };
+
 class Solution {
 public:
-	ListNode* ReverseList(ListNode* pHead) {
-		struct ListNode *next = NULL;
-		struct ListNode *prv = NULL;
-		while (pHead)
-		{
-			next = pHead->next;
-			pHead->next = prv;
-			prv = pHead;
-			pHead = next;
+	/*
+		//三个指针
+		ListNode* ReverseList(ListNode* pHead) {
+			if(pHead == NULL || pHead->next == NULL)
+				return pHead;
+
+			ListNode* prev = pHead;
+			ListNode* cur = pHead->next;
+			ListNode* next = cur->next;
+
+			while(next != NULL){
+				//翻转
+				cur->next = prev;
+				prev = cur;
+				cur = next;
+				next = next->next;
+			}
+			cur->next = prev; //当传入的链表只有两个节点 or 上述翻转结束时，最后一个节点并未翻转
+			pHead->next = NULL;
+			pHead = cur;  //头指针指向最后一个节点
+			return pHead;
 		}
-		return prv;
+	*/
+	//头插法
+	ListNode* ReverseList(ListNode* pHead) {
+		if (pHead == NULL || pHead->next == NULL)
+			return pHead;
+
+		ListNode* newhead = NULL;
+		while (pHead != NULL) {
+			//保存下一个结点
+			ListNode* p = pHead;
+			pHead = pHead->next;
+
+			//头插新结点
+			p->next = newhead;
+			newhead = p;
+		}
+		return newhead;
 	}
 };
+
+
+
