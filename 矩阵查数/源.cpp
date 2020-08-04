376f1525d3dfc90a9118da8e1771c8f43d2d4e36
ades4 <1647635529@qq.com>
2020-08-04 22:19:51 +0800

    二维数组里的查找

--- 矩阵查数/源.cpp
+++ 矩阵查数/源.cpp
@@ -3,6 +3,31 @@
 #include <vector>
 using namespace std;
 
+bool Find(int target, vector<vector<int> > array) {
+	// 1 2 3 4
+	// 2 3 4 5
+	// 3 4 5 6
+	// 4 5 6 7      //4
+	int i = 0;
+	int j = array[0].size() - 1;
+
+	while (i < array.size() && j >= 0) {
+		if (array[i][j] > target) {
+			//array[i][j]一定是当前行（竖着）最大的，当前列最小的
+			//排除当前列
+			j--;
+		}
+		else if (array[i][j] < target) {
+			//排除当前行
+			i++;
+		}
+		else {
+			return true;
+		}
+	}
+	return false;
+}
+
 int main()
 {
 	int n, c, x, key;
